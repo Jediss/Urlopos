b425723d44e3617c3089577b2ba4539d091c26f2
Jediss <kubaczobot@gmail.com>
2025-10-03 20:58:56 +0200

    Initial commit 1. Compose Compiler Gradle plugin initial configuration from "https://developer.android.com/develop/ui/compose/compiler" 2. Jetpack Compose initial configuration from "https://developer.android.com/develop/ui/compose/setup#kotlin"

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.example.urlopos_v1"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.urlopos_v1"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    val composeBom = platform("androidx.compose:compose-bom:2025.09.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    //Material Design 3
+    implementation("androidx.compose.material3:material3")
+
+    //Android Studio Preview support
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    //UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Window size utils
+    implementation("androidx.compose.material3.adaptive:adaptive")
+
+    //Integration with activities
+    implementation("androidx.activity:activity-compose:1.11.0")
+
+    //Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+
+    //Integration with LiveData
+    implementation("androidx.compose.runtime:runtime-livedata")
+
+    //Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+
+}
